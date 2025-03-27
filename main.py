380c99b1624b6031132b484ce4d5b1ef27b411d4
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-27 15:09:54 +0900

    Creating the labels for the invoice document

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 # Invoice Automation System
 # An Invoice is basically a request for payment
+import os 
+import subprocess
+import tkinter as tk 
+import datetime as dt 
+from tkinter import filedialog
+from tkinter import messagebox
+import docx 
+
+class InvoiceAutomation:
+
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Invoice Automation")
+        self.root.geometry("500x600")
+
+        self.partner_label = tk.Label(self.root, text="Partner")
+        self.partner_label = tk.Label(self.root, text="Partner")
+        self.partner_label = tk.Label(self.root, text="Partner")
+        self.partner_label = tk.Label(self.root, text="Partner")
+        self.partner_label = tk.Label(self.root, text="Partner")
+        self.partner_label = tk.Label(self.root, text="Partner")
+        self.partner_label = tk.Label(self.root, text="Partner")
